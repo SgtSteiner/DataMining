b92f137a5e40fcffcd32b6ca30fbaedffc016453
Steiner <futitotal@gmail.com>
2018-07-20 13:59:22 +0200

    nearest neighbour refactoring

--- nearest_neighbour.py
+++ nearest_neighbour.py
@@ -3,7 +3,10 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.pipeline import Pipeline
 from matplotlib import pyplot as plt
+from collections import defaultdict
 
 
 if __name__ == "__main__":
@@ -54,3 +57,46 @@ if __name__ == "__main__":
 
     plt.plot(parameters_value, avg_scores, "-o")
     plt.show()
+
+    for parameter, scores in zip(parameters_value, all_scores):
+        n_scores = len(scores)
+        plt.plot([parameter] * n_scores, scores, "-o")
+    plt.show()
+
+    plt.plot(parameters_value, all_scores, 'bx')
+    plt.show()
+
+    # Para compensar la varianza ejecutamos 100 veces por cada parámetro de n_neighbors
+    # all_scores = defaultdict(list)
+    # parameter_values = list(range(1, 21))
+    # for n_neighbors in parameter_values:
+    #     for i in range(100):
+    #         estimator = KNeighborsClassifier(n_neighbors=n_neighbors)
+    #         scores = cross_val_score(estimator, X, y, scoring='accuracy', cv=10)
+    #         all_scores[n_neighbors].append(scores)
+    # for parameter in parameter_values:
+    #     scores = all_scores[parameter]
+    #     n_scores = len(scores)
+    #     plt.plot([parameter] * n_scores, scores, '-o')
+    # plt.show()
+
+    # Preprocesamiento
+    X_broken = np.array(X)
+    X_broken[:, ::2] /= 10  # Rompemos el dataset dividiendo cada segunda característica por 10
+    estimator = KNeighborsClassifier()
+    original_scores = cross_val_score(estimator, X, y, scoring='accuracy')
+    print("The original average accuracy for is {0: .1f} % ".format(np.mean(original_scores) * 100))
+    broken_scores = cross_val_score(estimator, X_broken, y, scoring='accuracy')
+    print("The 'broken' average accuracy for is {0: .1f} % ".format(np.mean(broken_scores) * 100))
+
+    # Preprocesamiento estándar
+    X_transformed = MinMaxScaler().fit_transform(X_broken)
+    estimator = KNeighborsClassifier()
+    transformed_scores = cross_val_score(estimator, X_transformed, y, scoring="accuracy")
+    print("The transformed average accuracy for is {0: .1f} % ".format(np.mean(transformed_scores) * 100))
+
+    # Pipelines
+    scaling_pipeline = Pipeline([("scale", MinMaxScaler()),
+                                 ("predict", KNeighborsClassifier())])
+    scores = cross_val_score(scaling_pipeline, X_broken, y, scoring="accuracy")
+    print("The pipeline scored average accuracy for is {0: .1f} % ".format(np.mean(scores) * 100))
